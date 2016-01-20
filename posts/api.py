0607bc195d29fcfcd54dc7e3499aaa7c16086108
Wil DAmato <wil.damato@gmail.com>
2016-01-20 04:59:06 +0000

    initial posts commit with delete post tests

--- posts/api.py
+++ posts/api.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import request, Response, url_for
+from flask import request, Response, url_for, redirect
 from jsonschema import validate, ValidationError
 
 from . import models
@@ -8,3 +8,48 @@ from . import decorators
 from posts import app
 from .database import session
 
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+    """ Get a list of posts """
+    
+    # Get the posts from the database
+    posts = session.query(models.Post).order_by(models.Post.id)
+    
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def post_get(id):
+    """ Single post endpoint """
+    # Get the post from the database
+    post = session.query(models.Post).get(id)
+
+    # Check whether the post exists
+    # If not return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    # Return the post as JSON
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["DELETE"])
+@decorators.accept("application/json")
+def post_delete(id):
+    """ Delete Post """ 
+    # Get the post from the database
+    post = session.query(models.Post).get(id)
+    
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+    else:
+      session.delete(post)
+      session.commit()
+      return redirect(url_for("posts_get"))
